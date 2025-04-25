4d804509d67c63bfddc5a9cea624eaaffd1e8d8b
hau.le <hau.le@decision-science.com>
2025-04-25 14:37:17 +0700

    Push code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	optimizeDeps: {
+		esbuildOptions: {
+			define: {
+				global: 'globalThis',
+			},
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true,
+				}),
+				NodeModulesPolyfillPlugin(),
+			],
+		},
+	},
+	resolve: {
+		alias: {
+			crypto: 'crypto-browserify',
+		},
+	},
 })
